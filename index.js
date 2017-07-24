867099d141b60663c7d0e1faa3cd4041ff7bf5fb
Ugur Unlu <ugur.unlu@haselt.com>
2017-07-24 15:42:37 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = run;
+
+function run(model){
+	switch(model)
+	{
+		case "488":
+		console.log("3.9-litre, 7-speed, V8 twin-turboharged");
+		break;
+		default:
+		console.log("VROOOOM!");
+	}
+}
